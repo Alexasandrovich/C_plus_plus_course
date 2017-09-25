6228116dddcaa1b06b669b1d0bf7b074f672f4a4
Alexasandrovich <taganrog.98@mail.ru>
2017-09-25 20:21:30 +0300

    firts commit

---
+++ complex.h
@@ -0,0 +1,32 @@
+#ifndef COMPLEX_H_20170921
+#define COMPLEX_H_20170921
+
+#include <iosfwd> 
+
+struct Complex {
+    Complex() {}
+    explicit Complex(const double real);
+    Complex(const double real, const double imaginary);
+    bool operator==(const Complex& rhs) const;
+    bool operator!=(const Complex& rhs) const;
+    Complex& operator+=(const Complex& rhs);
+    Complex& operator+=(const double rhs);
+    Complex& operator-=(const Complex& rhs);
+    Complex& operator-=(const double rhs);
+    Complex& operator*=(const Complex& rhs);
+    Complex& operator*=(const double rhs);
+    std::ostream& writeTo(std::ostream& ostrm) const;
+    std::istream& readFrom(std::istream& istrm);
+
+    double re{ 0.0 };
+    double im{ 0.0 };
+
+    static const char leftBrace{ '{' };
+    static const char separator{ ',' };
+    static const char rightBrace{ '}' };
+
+};
+std::ostream& operator << (std::ostream& ostrm, const Complex& rhs);
+std::istream& operator >> (std::istream& istrm, Complex& rhs);
+
+#endif
